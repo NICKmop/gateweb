5b9e682540d40fbb80e0548886b198350986c605
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-07-25 15:50:36 +0900

    commit

--- static/js/default.js
+++ static/js/default.js
@@ -2,6 +2,12 @@
 //  custom
 // -----------------------------
 const btnArr = document.getElementsByTagName('button');
+const navLink = document.getElementsByClassName('nav-link');
+
+function menuBoxScroll (className) {
+    console.log("className : ", className);
+    return document.getElementById(className);
+}
 
 for(let i = 0; i < btnArr.length; i++){
     btnArr[i].addEventListener('click',function(e){
@@ -11,4 +17,18 @@ for(let i = 0; i < btnArr.length; i++){
             document.getElementById('service-2-scroll').scrollLeft += 450;
         }
 });
+}
+for (let i = 0; i < navLink.length; i++)     {
+    navLink[i].addEventListener('click', function(params) {
+       console.log("click : ", this.text); 
+       if (this.text == 'About Us') {
+        window.scrollBy({top: menuBoxScroll('aboutUs').getBoundingClientRect().top, behavior: 'smooth'})
+       }else if(this.text == '메인페이지'){
+        window.scrollBy({top: menuBoxScroll('navbarDropdown').getBoundingClientRect().top, behavior: 'smooth'})
+       }else if(this.text == 'Contact'){
+        window.scrollBy({top: menuBoxScroll('Contact').getBoundingClientRect().top, behavior: 'smooth'})
+       }
+
+       
+    });
 }
